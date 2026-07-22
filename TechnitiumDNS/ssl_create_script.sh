e32f30edc2f4eacdeada44ad1650019834f0144b
abcoit <abco.it@abcoresearch.com>
2026-07-22 10:11:18 -0500

    Rename script from convert_PKCS12.sh to sslconvert.sh

--- TechnitiumDNS/ssl_create_script.sh
+++ TechnitiumDNS/ssl_create_script.sh
@@ -9,14 +9,14 @@ echo "What should be certificate password be? (press enter for none)"
 read PFX_PASSWORD
 
 # Write the .sh file
-/bin/cat <<EOM >/etc/letsencrypt/renewal-hooks/deploy/convert_PKCS12.sh
+/bin/cat <<EOM >/etc/letsencrypt/renewal-hooks/deploy/sslconvert.sh
 #!/bin/sh
 sudo openssl pkcs12 -export -out "/etc/dns/$KEY_DOMAIN.pfx" -inkey /etc/letsencrypt/live/$KEY_DOMAIN/privkey.pem -in /etc/letsencrypt/live/$KEY_DOMAIN/cert.pem -certfile /etc/letsencrypt/live/$KEY_DOMAIN/chain.pem -passout pass:$PFX_PASSWORD
 echo "PKCS12 file written to /etc/dns/$KEY_DOMAIN.pfx"
 EOM
 
 # Make script executable.
-sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/convert_PKCS12.sh
+sudo chmod +x /etc/letsencrypt/renewal-hooks/deploy/sslconvert.sh
 
 # Run the script manually for the first time to generate the initial .pfx file
-sudo /etc/letsencrypt/renewal-hooks/deploy/convert_PKCS12.sh
+sudo /etc/letsencrypt/renewal-hooks/deploy/sslconvert.sh
