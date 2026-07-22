785070f607710957f5ea54caefdacb6dc7b08128
abcoit <abco.it@abcoresearch.com>
2026-07-22 09:05:12 -0500

    Add sudo to openssl command in sslconvert.sh

--- TechnitiumDNS/sslconvert.sh
+++ TechnitiumDNS/sslconvert.sh
@@ -11,7 +11,7 @@ read PFX_PASSWORD
 # Write the .sh file
 /bin/cat <<EOM >/etc/letsencrypt/renewal-hooks/deploy/convert_PKCS12.sh
 #!/bin/sh
-openssl pkcs12 -export -out "/etc/dns/$KEY_DOMAIN.pfx" -inkey /etc/letsencrypt/live/$KEY_DOMAIN/privkey.pem -in /etc/letsencrypt/live/$KEY_DOMAIN/cert.pem -certfile /etc/letsencrypt/live/$KEY_DOMAIN/chain.pem -passout pass:$PFX_PASSWORD
+sudo openssl pkcs12 -export -out "/etc/dns/$KEY_DOMAIN.pfx" -inkey /etc/letsencrypt/live/$KEY_DOMAIN/privkey.pem -in /etc/letsencrypt/live/$KEY_DOMAIN/cert.pem -certfile /etc/letsencrypt/live/$KEY_DOMAIN/chain.pem -passout pass:$PFX_PASSWORD
 echo "PKCS12 file written to /etc/dns/$KEY_DOMAIN.pfx"
 EOM
 
